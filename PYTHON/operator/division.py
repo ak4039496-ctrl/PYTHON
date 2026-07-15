3280d08c0d44c773eed6644c331b677a8f2dbffb
AMIT--GUPTA <ak4039496@gmail.com>
2026-07-15 12:16:30 +0530

    Division

---
+++ PYTHON/operator/division.py
@@ -0,0 +1,12 @@
+# Author:-Amit Kumar
+# Date:- 2026-07-10
+
+# Ask user for inputs
+a = int(input("Enter your no. :- "))
+b = int(input("Enter your second no. :- "))
+
+# Perform integer division using // (matches C's standard int division)
+divi = a // b
+
+# Print result of division
+print(f"a/b Divi:-  {divi}")
