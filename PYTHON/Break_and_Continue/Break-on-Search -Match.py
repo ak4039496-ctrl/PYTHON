71a06880ded882684ae718dae866720216b1317d
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-02 21:43:29 +0530

    Break-on-Search -Match

---
+++ PYTHON/Break_and_Continue/Break-on-Search -Match.py
@@ -0,0 +1,10 @@
+# Author: Amit Gupta
+# Date: 15-07-2026
+# Description: Stops when target number found
+
+nums = [10, 20, 30, 40, 50]
+target = 30
+for n in nums:
+    if n == target:
+        print("Found:", n)
+        break
