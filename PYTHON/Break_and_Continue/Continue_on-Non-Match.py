069d42b520ae7bf73cbac3bf9e8b79d89cb259f7
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-03 21:31:36 +0530

    Continue_on-Non-Match

---
+++ PYTHON/Break_and_Continue/Continue_on-Non-Match.py
@@ -0,0 +1,10 @@
+# Author: Amit Gupta
+# Date: 16-07-2026
+# Description: Prints only matching numbers
+
+nums = [10, 20, 30, 40, 50]
+target = 30
+for n in nums:
+    if n != target:
+        continue
+    print("Match:", n)
