42c26425e906186b9869a90523bf10c7348ef4e8
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-07 20:18:43 +0530

    equal_check_ternary

---
+++ PYTHON/operator/equal_check_ternary.py
@@ -0,0 +1,9 @@
+# Author:-Amit Kumar
+# Date:- 2026-07-10
+
+# Input two numbers
+a = int(input("Enter 1st numbers:- "))
+b = int(input("Enter second no.:- "))
+
+# Check equality using simple Python conditional expression
+print("Numbers are Equal" if a == b else "Numbers are NOT Equal")
