d83d0d751e68e771675b1a988350456266cc65ed
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-07 20:27:38 +0530

    relational_demo

---
+++ PYTHON/operator/relational_demo.py
@@ -0,0 +1,25 @@
+# Author:-Amit Kumar
+# Date:- 2026-07-10
+
+# Input two numbers
+a = int(input("Enter first number:- "))
+b = int(input("Enter second number:- "))
+
+# Demonstrate relational operators
+if a > b:   # Greater than
+    print(a, "is greater than", b)
+
+if a < b:   # Less than
+    print(a, "is less than", b)
+
+if a >= b:  # Greater than or equal to
+    print(a, "is greater than or equal to", b)
+
+if a <= b:  # Less than or equal to
+    print(a, "is less than or equal to", b)
+
+if a == b:  # Equal to
+    print(a, "is equal to", b)
+
+if a != b:  # Not equal to
+    print(a, "is not equal to", b)
