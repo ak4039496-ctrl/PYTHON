4a4c5fd7e5d99cefc5f4c7231a1b98b4fd381457
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-07 20:20:02 +0530

    if_first_code

---
+++ PYTHON/operator/if_first_code.py
@@ -0,0 +1,9 @@
+# Author:-Amit Kumar
+# Date:- 2026-07-10
+
+# Declare an integer variable and assign value
+number = 10
+
+# Check if number is greater than 0
+if number > 0:
+    print("Number is positive")  # Executes only if condition is true
