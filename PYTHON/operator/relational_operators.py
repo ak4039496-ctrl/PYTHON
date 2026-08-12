2865c16ca08085e169ef6c1224bdd46708dcebe0
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-12 19:37:18 +0530

    relational_operators

---
+++ PYTHON/operator/relational_operators.py
@@ -0,0 +1,14 @@
+# Author:-Amit Kumar
+# Date:- 2026-07-10
+
+# Declare variables
+x = 5
+y = 10
+
+# Python expressions return True/False. int() converts them to 1/0 to match C output.
+print("x > y :-", int(x > y))   # Greater than
+print("x < y :-", int(x < y))   # Less than
+print("x == y :-", int(x == y)) # Equal to
+print("x != y :-", int(x != y)) # Not equal to
+print("x >= y :-", int(x >= y)) # Greater than or equal to
+print("x <= y :-", int(x <= y)) # Less than or equal to
