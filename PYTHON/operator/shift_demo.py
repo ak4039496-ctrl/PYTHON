41bd15c8f66a6e6f76662a010e991c2fef3752ed
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-12 19:37:57 +0530

    shift_demo

---
+++ PYTHON/operator/shift_demo.py
@@ -0,0 +1,13 @@
+# Author:-Amit Kumar
+# Date:- 2026-07-10
+
+# Input a number
+num = int(input("Enter a number:- "))
+
+# Left shift operator (multiply by 2 for each shift)
+print("num << 1 =", num << 1)   # Shift left by 1 bit
+print("num << 2 =", num << 2)   # Shift left by 2 bits
+
+# Right shift operator (divide by 2 for each shift)
+print("num >> 1 =", num >> 1)   # Shift right by 1 bit
+print("num >> 2 =", num >> 2)   # Shift right by 2 bits
