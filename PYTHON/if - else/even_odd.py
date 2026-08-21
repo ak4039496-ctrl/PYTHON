4970c4063f4b9dd75c7458aa9122e983890f37ea
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-21 19:47:57 +0530

    even_odd

---
+++ PYTHON/if - else/even_odd.py
@@ -0,0 +1,14 @@
+# Author: Amit Gupta
+# Date: 18-07-2026
+# Description:- Program to check whether a number is even or odd.
+
+# Input from user
+num = int(input("Enter a number:- "))
+
+# If condition to check even number
+if num % 2 == 0:
+    print(f"{num} is Even")
+# Else condition for odd number
+else:
+    print(f"{num} is Odd")
+    
