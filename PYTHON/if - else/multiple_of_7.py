a8d4ab7825de79159e52aa14aad46c9d8f933e9e
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-21 19:51:02 +0530

    multiple_of_7

---
+++ PYTHON/if - else/multiple_of_7.py
@@ -0,0 +1,11 @@
+# Author: Amit Gupta
+# Date: 18-07-2026
+
+# Variable to store the number
+num = int(input("Enter a number:- "))
+
+# Check if number is divisible by 7
+if num % 7 == 0:
+    print(f"{num} is a multiple of 7")
+else:
+    print(f"{num} is NOT a multiple of 7")
