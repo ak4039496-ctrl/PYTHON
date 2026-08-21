694921369c3795caf2d97ed0d7ecb8d8469fed42
AMIT--GUPTA <ak4039496@gmail.com>
2026-08-21 19:46:59 +0530

    driving_license

---
+++ PYTHON/if - else/driving_license.py
@@ -0,0 +1,14 @@
+# Author: Amit Gupta
+# Date: 18-07-2026
+
+# Ask user to enter age
+age = int(input("Enter your age:- "))
+
+# Print entered age
+print(f"You entered:- {age}")
+
+# Driving license eligibility logic
+if age >= 18:
+    print("You are eligible for a Driving License")
+else:
+    print("You are NOT eligible for a Driving License")
